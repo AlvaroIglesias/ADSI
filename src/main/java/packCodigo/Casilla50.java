dcace67f100b22099a4783bce1ea0a1e622952ef
Jon <jonmiberrio3b@gmail.com>
2020-12-27 11:24:39 +0100

    Actualización minas especiales (2)
    -Añadidos los iconos de las minas especiales (Resubido)

---
+++ src/main/java/packCodigo/Casilla50.java
@@ -0,0 +1,23 @@
+
+package packCodigo;
+
+public class Casilla50 extends Casilla{
+	
+	public Casilla50(){
+		super();
+	}
+	
+	public void descubrir(){
+		super.descubrir();
+	}
+	
+	public void inicializar(String coor){
+		super.inicializar(coor);
+	}
+	
+	public void cambioBandera(){
+		super.cambioBandera();
+	}
+	
+	
+}
---
+++ src/main/java/packCodigo/CasillaReset.java
@@ -0,0 +1,23 @@
+
+package packCodigo;
+
+public class CasillaReset extends Casilla{
+	
+	public CasillaReset(){
+		super();
+	}
+	
+	public void descubrir(){
+		super.descubrir();
+	}
+	
+	public void inicializar(String coor){
+		super.inicializar(coor);
+	}
+	
+	public void cambioBandera(){
+		super.cambioBandera();
+	}
+	
+	
+}
